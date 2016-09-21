e0e75d89759eea95027d9f3f179dab0a158932d9
Joe Ward <jward@leapmotion.com>
2016-09-21 13:51:02 -0700

    hacking...

--- drivers/media/platform/bcm2835/leap_xu_v4l2.h
+++ drivers/media/platform/bcm2835/leap_xu_v4l2.h
@@ -6,6 +6,13 @@
 #ifndef LEAP_XU_V4L2_H
 #define LEAP_XU_V4L2_H
 
+#include <media/v4l2-device.h>
+#include <media/v4l2-ioctl.h>
+#include <media/v4l2-ctrls.h>
+#include <media/v4l2-fh.h>
+#include <media/v4l2-event.h>
+#include <media/v4l2-common.h>
+
 static long leap_xu_ioctl_default(struct file *file, void *fh, bool valid_prio, unsigned int cmd, void *arg);
 
 #endif /* LEAP_XU_V4L2_H */
