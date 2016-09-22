b6ec5143e014dee35872bfea7820136d953187c7
Joe Ward <jward@leapmotion.com>
2016-09-22 13:22:03 -0700

    added build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+KERNEL=kernel
+make bcmrpi_defconfig
+make -j4 zImage modules dtbs
+sudo make modules_install
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+sudo cp arch/arm/boot/dts/overlays/README /boot/overlays/
+sudo scripts/mkknlimg arch/arm/boot/zImage /boot/$KERNEL.img
